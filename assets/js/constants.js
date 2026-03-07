88a7cff35e1981079f05d4e6493f665a817e0cfc
huycozy <huy@nevercode.io>
2026-03-07 10:25:13 +0700

    update play store url

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,5 +1,5 @@
 const DOWNLOAD_URLS = {
-    android: "https://github.com/NetShareOSS/netshare/releases",
+    android: "https://play.google.com/store/apps/details?id=com.app.netshare",
     ios: "https://apps.apple.com/vn/app/id6758239332",
     macos: "https://apps.apple.com/vn/app/id6758239332"
 };
